8ccbf9a55411c76ff2c268aceb1c0b7c9974a087
Brian Sunday <brians931@gmail.com>
2021-01-24 21:54:33 +0300

    Updates

---
+++ precache-manifest.2598bdb134d900bdbd4e3a3dabec06a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228bfc02258d2e4153461129827b0317",
+    "url": "/sitematching/index.html"
+  },
+  {
+    "revision": "516c6ace42cf819d6912",
+    "url": "/sitematching/static/css/2.69ac34f2.chunk.css"
+  },
+  {
+    "revision": "b6c443bc522e84331b54",
+    "url": "/sitematching/static/css/main.1124fcef.chunk.css"
+  },
+  {
+    "revision": "516c6ace42cf819d6912",
+    "url": "/sitematching/static/js/2.1a04cbf4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sitematching/static/js/2.1a04cbf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c443bc522e84331b54",
+    "url": "/sitematching/static/js/main.8300710b.chunk.js"
+  },
+  {
+    "revision": "22798d8843d5052a5f77",
+    "url": "/sitematching/static/js/runtime-main.d5a27362.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/sitematching/precache-manifest.0565fcec760e5065bec3426c9f4c00ac.js"
+  "/sitematching/precache-manifest.2598bdb134d900bdbd4e3a3dabec06a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/sitematching/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sitematching/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
